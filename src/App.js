ad92c8668be8a93af7b1cdfc6feb2314135837fb
Greg Testo <gregt@pursuit.org>
2021-11-12 15:55:07 -0500

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      count: 0,
+      numInput: ""
+    }
+  }
+
+  handleDecrement=(num)=>{
+    this.setState({
+      count: this.state.count-num
+    })
+  }
+  handleIncrement=(num)=>{
+    this.setState({
+      count: this.state.count+num
+    });
+  }
+
+  handleInput=(event)=>{
+    this.setState({
+      numInput: event.target.value
+    })
+  }
+
+  handleReset=()=>{
+    this.setState({
+      count: 0
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        <div>{ this.state.count }</div>
+        <button onClick={this.handleReset}>Reset</button>
+        <button onClick={()=>this.handleIncrement(Number(this.state.numInput))}>+Custom</button>
+        <button onClick={()=>this.handleIncrement(10)}>+10</button>
+        <button onClick={()=>this.handleIncrement(1)}>+</button>
+        <button onClick={()=>this.handleDecrement(1)}>-</button>
+        <button onClick={()=>this.handleDecrement(10)}>-10</button>
+        <button onClick={()=>this.handleDecrement(Number(this.state.numInput))}>-Custom</button>
+
+        <br />
+        <input 
+          type="number"
+          value={this.state.numInput}
+          onInput={this.handleInput}
+          placeholder="Value"
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
